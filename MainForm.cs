2c6c350b25d052324b501d49e94da030e8ec6a1d
Russell <russofat@gmail.com>
2023-05-28 14:25:55 +0300

    Updated forms

--- MainForm.cs
+++ MainForm.cs
@@ -18,19 +18,28 @@ namespace CourseWork_TP_2023
 {
     public partial class MainForm : Form
     {
-        public RemoteObjectHTTP remoteHTTP;
+        public RemoteObjectHTTP remoteHTTP; //remote object
         HttpChannel channel = new HttpChannel(new Dictionary<string, string> { { "port", "0" } }, new BinaryClientFormatterSinkProvider(), new BinaryServerFormatterSinkProvider { TypeFilterLevel = TypeFilterLevel.Full });
 
         public MainForm()
         {
             InitializeComponent();
-            ChannelServices.RegisterChannel(channel, false);
+            ConnectToServer();
+            InitBrands();
+        }
+
+        private void ConnectToServer() // установка соединения с сервером
+        {
+            if (ChannelServices.GetChannel("http") == null)
+            {
+                ChannelServices.RegisterChannel(channel, false);
+            }
             remoteHTTP = new RemoteObjectHTTP();
             ILease lease = (ILease)remoteHTTP.InitializeLifetimeService();
             ClientSponsor clientHTTPSponsor = new ClientSponsor();
             lease.Register(clientHTTPSponsor);
-            InitBrands();
         }
+
 
         private void InitBrands()
         {
@@ -58,34 +67,20 @@ namespace CourseWork_TP_2023
             DataTable guitar = remoteHTTP.findByModel(ComboBoxModel.SelectedItem.ToString());
             foreach (DataRow row in guitar.Rows)
             {
-                brand_label.Text = "Производитель: " + row["Brand"].ToString();
-                model_label.Text = "Модель: " + row["Model"].ToString();
+                brand_label.Text = "Производитель:  " + row["Brand"].ToString();
+                model_label.Text = "Модель:  " + row["Model"].ToString();
                 articul_label.Text = "Производитель: " + row["Articul"].ToString();
-                price_label.Text = "Цена: " + row["Price"].ToString() + "р.";
+                price_label.Text = "Цена:  " + row["Price"].ToString() + "р.";
                 descriptionBox.Text = row["Description"].ToString();
                 imageBox.Image = new Bitmap(row["Image"].ToString());
-            }
+            } 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {/*
-            using (SQLiteConnection conn = new SQLiteConnection("Data Source=C:/Users/Руслан/source/repos/CourseWork_TP_WinForms/Server/DB_CourseWork.db"))
-            {
-                conn.Open();
-
-                string sql = "SELECT * FROM users";
-                SQLiteCommand cmd = new SQLiteCommand(sql, conn);
-
-                SQLiteDataReader dataReader = cmd.ExecuteReader();
-
-                while(dataReader.Read())
-                {
-                    listBox1.Items.Add(string.Format("{0}, {1}, {2}", dataReader.GetInt32(0), dataReader.GetString(1), dataReader.GetString(2)));
-                }
-
-                conn.Close();
-
-            }*/
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
         }
     }
 }
